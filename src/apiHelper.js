a13458bc4401249c4074ea41ba5a3260d86e3bce
hzane <darkdirk1@gmail.com>
2018-12-10 05:24:29 -0600

    ya

--- src/apiHelper.js
+++ src/apiHelper.js
@@ -9,7 +9,7 @@ const terms = require('./terms');
 let {wrapRow, wrapItems, wrapHTML} = require('./createHTML.js');
 
 function handleRequest(termsAll, req, response){
-	return 'ok';
+	response.send('ok');
 	var counter=0;
 	var htmlAll='';
 	var endDate = typeof req.query.t === 'string' ? calcEndDate(req.query.t) : calcEndDate('day');
