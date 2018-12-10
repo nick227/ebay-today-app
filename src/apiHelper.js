473c9d5cabbdc4cd7ff939535daed0771ab527a3
hzane <darkdirk1@gmail.com>
2018-12-10 06:32:36 -0600

    ya

--- src/apiHelper.js
+++ src/apiHelper.js
@@ -37,7 +37,6 @@ function generate(ebay, terms, counter, callback){
 	async.mapSeries(terms, (value, next) => {
 		ebay.findItemsByKeywords(value).then((data) => {
 			let html = '';
-			console.log(JSON.stringify(data[0].errorMessage));
 			if(typeof data[0]==='object' && typeof data[0].searchResult[0].item==='object'){
 				let matches = data[0].searchResult[0].item;
 				if(matches.length){
